78c933d3461e74f7b788c79d492a6eb9177ed431
Britton Perry <bper5656@740s-pl-132.local>
2016-09-12 09:56:26 -0600

    We dealt with numbers today

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class StupidController
 {
-	private static String myName;
+	private String myName;
 	private Scanner inputReader;
 
 	public StupidController()
@@ -26,5 +26,17 @@ public class StupidController
 		System.out.println("how is your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Oh, you said: " + input + " about your day ");
-	}
+		System.out.println("My next question is this: What is the best type of food?");
+		String foodinput = inputReader.nextLine();
+		System.out.println("Ok, you typed this: " + foodinput);
+	
+		System.out.println("What is the greatest game of all time?");
+		String gamergirlinput = inputReader.nextLine();
+		System.out.println("While you may think: " + gamergirlinput + " is the best game but actually it is not " );
+		
+		System.out.println("woahheyguyswelcometoebgames");
+		int someInput = inputReader.nextInt();
+		System.out.println("Ok, you typed this: " + someInput);
+	}	
+	
 }	
